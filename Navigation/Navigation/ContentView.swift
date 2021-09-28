aba810785036ba05cacdf9758de25755bb4430e9
kwanghyun.won <wonkwh@gmail.com>
2021-09-28 12:06:08 +0900

    replace index to enum value

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -6,10 +6,17 @@
 //
 
 import SwiftUI
+
+enum Tab {
+  case one
+  case two
+  case three
+}
+
 class AppViewModel: ObservableObject {
-  @Published var selectedTab: Int
+  @Published var selectedTab: Tab
   
-  init(selectedTab: Int = 1) {
+  init(selectedTab: Tab = .one) {
     self.selectedTab = selectedTab
   }
 }
@@ -22,24 +29,24 @@ struct ContentView: View {
     TabView(selection: self.$viewModel.selectedTab ) {
       
       Button("Go to 2nd Tab") {
-        self.viewModel.selectedTab = 2
+        self.viewModel.selectedTab = .two
       }
       .tabItem { Text("One") }
-      .tag(1)
+      .tag(Tab.one)
     
       Text("Two")
         .tabItem { Text("Two") }
-        .tag(2)
+        .tag(Tab.two)
       
       Text("Three")
         .tabItem { Text("Three") }
-        .tag(3)
+        .tag(Tab.three)
     }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView(viewModel: .init())
+    ContentView(viewModel: .init(selectedTab: .two))
   }
 }
